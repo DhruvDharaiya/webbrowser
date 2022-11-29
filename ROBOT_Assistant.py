cb9c2fa22dedde0150fa664ebdb62a1151bf332d
PantherProgramming <117986885+DhruvDharaiya@users.noreply.github.com>
2022-11-29 16:17:05 +0530

    Add files via upload

---
+++ ROBOT_Assistant.py
@@ -0,0 +1,47 @@
+# import module called webbrowser
+import webbrowser as wb
+
+# query = input('SEARCH ON GOOGLE: ')
+# try:
+#     query = query.replace(' ', '+')
+# except:
+#     query = query
+
+# use wb
+
+# make function for wb
+
+# def FWb():
+#     linkBR = f'https://www.google.com/search?q={query}'
+#     wb.open(linkBR)
+
+# Call Function FWb()
+# object = FWb()
+# Function Called
+
+# Example search : how to grow tomatos
+# Replaced       : how+to+grow+tomatos
+
+
+'''
+search on youtube now !
+'''
+queryYOUTUBE = input('SEARCH ON YOUTUBE : ')
+queryYOUTUBE.replace(' ', '+')
+def FYoutube():
+    linkBRY = f'https://www.youtube.com/results?search_query={queryYOUTUBE}'
+    wb.open(linkBRY)
+object = FYoutube()
+# RUN YOUTUBE ONLY
+# Change Variable Names, function names
+
+
+'''
+
+Thanks For Watching 
+Source Code In Describsion BoX
+
+
+'''
+
+
